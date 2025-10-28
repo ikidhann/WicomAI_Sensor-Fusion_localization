1f725934e6fb5dc3204843fb79edb22988307963
alfialdo <aldoalfi02@gmail.com>
2025-10-28 16:26:34 +0900

    build: add Dockerfile for building koide3 calib with superglue

---
+++ koide3/koide3_calib.sh
@@ -0,0 +1,72 @@
+bag_path=/home/wicomai-cv/Documents/sensor_fusion/ros2-drone-localization/bags/rosbag2_2025_10_23-22_40_30
+preprocessed_path=/home/wicomai-cv/Documents/sensor_fusion/ros2-drone-localization/bags_preprocessed
+docker build -t koide3:jazzy -f docker/jazzy/Dockerfile_with_superglue .
+
+# Preprocessing
+docker run \
+  --rm \
+  --net host \
+  --gpus all \
+  -e DISPLAY=$DISPLAY \
+  -v $HOME/.Xauthority:/root/.Xauthority \
+  -v $bag_path:/tmp/input_bags \
+  -v $preprocessed_path:/tmp/preprocessed \
+  koide3:jazzy \
+  ros2 run direct_visual_lidar_calibration preprocess -d \
+    --image_topic /sync/raw_image \
+    --points_topic /sync/velodyne_points \
+    --camera_info_topic /camera/cam_info \
+    /tmp/input_bags /tmp/preprocessed 
+
+# Initial guess
+# docker run \
+#   --rm \
+#   --net host \
+#   --gpus all \
+#   -e DISPLAY=$DISPLAY \
+#   -v $HOME/.Xauthority:/root/.Xauthority \
+#   -v $preprocessed_path:/tmp/preprocessed \
+#   koide3:jazzy \
+#   ros2 run direct_visual_lidar_calibration initial_guess_manual /tmp/preprocessed
+
+docker run \
+  --rm \
+  --net host \
+  --gpus all \
+  -e DISPLAY=$DISPLAY \
+  -v $HOME/.Xauthority:/root/.Xauthority \
+  -v $preprocessed_path:/tmp/preprocessed \
+  koide3:jazzy \
+  ros2 run direct_visual_lidar_calibration find_matches_superglue.py /tmp/preprocessed --rotate_camera 90
+
+docker run \
+  --rm \
+  --net host \
+  --gpus all \
+  -e DISPLAY=$DISPLAY \
+  -v $HOME/.Xauthority:/root/.Xauthority \
+  -v $preprocessed_path:/tmp/preprocessed \
+  koide3:jazzy \
+  ros2 run direct_visual_lidar_calibration initial_guess_auto /tmp/preprocessed
+
+# Fine registration
+docker run \
+  --rm \
+  --net host \
+  --gpus all \
+  -e DISPLAY=$DISPLAY \
+  -v $HOME/.Xauthority:/root/.Xauthority \
+  -v $preprocessed_path:/tmp/preprocessed \
+  koide3:jazzy \
+  ros2 run direct_visual_lidar_calibration calibrate /tmp/preprocessed
+
+# Result inspection
+docker run \
+  --rm \
+  --net host \
+  --gpus all \
+  -e DISPLAY=$DISPLAY \
+  -v $HOME/.Xauthority:/root/.Xauthority \
+  -v $preprocessed_path:/tmp/preprocessed \
+  koide3:jazzy \
+  ros2 run direct_visual_lidar_calibration viewer /tmp/preprocessed
